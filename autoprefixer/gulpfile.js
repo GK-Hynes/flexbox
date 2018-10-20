8897a1232b169a0709e1e158d7cda2ecfb98305a
Gerard Hynes <gkhynes@gmail.com>
2018-10-20 11:37:24 +0100

    Complete autoprefixer example

---
+++ autoprefixer/gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require("gulp");
+var autoprefixer = require("gulp-autoprefixer");
+
+gulp.task("styles", function() {
+  gulp
+    .src("css/style.css")
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("watch", function() {
+  gulp.watch("css/style.css", ["styles"]);
+});
